c33c5849655e0b5aa6c38756db28c92d643cbb49
burcineren <burcin.ern969@gmail.com>
2022-01-23 17:31:57 +0300

    fix: nodemon package added.

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    const photo = {
+        id: 5,
+        name: "Photo Name",
+        description: "Photo description",
+    }
+  res.send(photo);
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} una baglandi`);
+});
